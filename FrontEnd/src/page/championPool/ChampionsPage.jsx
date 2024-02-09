523babecd3e4aa806d75ee1004ddcfdd5dec20af
Gabriel Moda <gabriel-moda@hotmail.com>
2024-02-09 04:13:51 -0300

    [FEAT]

--- FrontEnd/src/page/championPool/ChampionsPage.jsx
+++ FrontEnd/src/page/championPool/ChampionsPage.jsx
@@ -9,11 +9,8 @@ import {
     TableRow,
     Paper,
     FormControl,
-    InputLabel,
-    Button,
     TextField,
-    Select,
-    MenuItem
+    Button
 } from '@mui/material';
 import Autocomplete from '@mui/material/Autocomplete';
 
@@ -45,7 +42,10 @@ const tierListOptions = [
     "D+", "D", "D-"
 ];
 
-// Componente para formatar os nomes de campeões
+// Lista de opções de lane
+const laneOptions = ["middle", "support", "bottom", "jungle", "top"];
+
+// Função para formatar os nomes de campeões
 const formatChampionNames = (names) => {
     return names.map(name => name.toLowerCase().replace(/\s+/g, ''));
 };
@@ -53,12 +53,18 @@ const formatChampionNames = (names) => {
 const ChampionsPage = () => {
     // Estados
     const [lane, setLane] = useState('middle');
-    const [selectedChampions, setSelectedChampions] = useState([]);
+    const [selectedChampions, setSelectedChampions] = useState(['anivia', 'annie', 'cassiopeia', 'syndra', 'viktor', 'vex', 'lux', 'malzahar', 'malphite', 'kled', 'orianna', 'ekko', 'cho`gath', 'kassadin', 'sylas', 'azir',
+        'ornn', 'maokai', 'kled', 'malphite', 'irelia', 'cho`gath', 'gwen', 'mordekaiser', 'shen', 'singed', 'anivia', 'garen', 'nasus', 'wukong', 'volibear',
+        'jarvaniv', 'kayn', 'karthus', 'maokai', 'ekko', 'warwick', 'nocturne', 'vi', 'volibear',
+        'karthus', 'cassiopeia', 'jhin', 'ashe', 'missfortune', 'swain', 'jinx', 'syndra', 'kog`maw', 'tristana', 'caitlyn', 'lucian',
+        'janna', 'soraka', 'annie', 'zilean', 'maokai', 'lux', 'leona', 'orianna'
+    ]);
     const [tierList, setTierList] = useState(["S+", "S", "S-", "A+", "A", "A-"]);
-    const [winRateNumericThreshold, setWinRateNumericThreshold] = useState(40);
+    const [winRateNumericThreshold, setWinRateNumericThreshold] = useState(51);
+    const [insertedChampions, setInsertedChampions] = useState([]);
 
     // Manipuladores de eventos
-    const handleChangeLane = (event) => setLane(event.target.value);
+    const handleChangeLane = (event, value) => setLane(value); // Atualize o estado da lane quando uma opção for selecionada
     const handleChangeSelectedChampions = (event, value) => setSelectedChampions(value || []);
     const handleChangeTiers = (event, value) => setTierList(value || []);
     const handleChangeWinRateNumericThreshold = (event) => setWinRateNumericThreshold(event.target.value);
@@ -67,8 +73,9 @@ const ChampionsPage = () => {
         try {
             console.log('Saving selected champions:', { lane, selectedChampions, tierList, winRateNumericThreshold });
             if (selectedChampions.length > 0) {
-                await axios.post('http://localhost:3001/selected-champions', { lane, selectedChampions, tierList, winRateNumericThreshold });
+                const response = await axios.post('http://localhost:3001/selected-champions', { lane, selectedChampions, tierList, winRateNumericThreshold });
                 console.log('Selected champions saved successfully!');
+                setInsertedChampions(response.data.champions); // Atualiza a lista de campeões inseridos
             } else {
                 console.log('No champions selected to save.');
             }
@@ -78,25 +85,19 @@ const ChampionsPage = () => {
     };
 
     return (
-        <div>
-            <h1>League of Legends Champion Statistics</h1>
-            <FormControl fullWidth>
-                <InputLabel id="lane-label">Select Lane</InputLabel>
-                <Select
-                    labelId="lane-label"
+        <div style={{ padding: '20px' }}>
+            <h1 style={{ color: '#FFA500', textAlign: 'center', fontFamily: 'Arial, sans-serif' }}>League of Legends Champion Statistics</h1>
+            <FormControl fullWidth style={{ marginBottom: '10px' }}>
+                <Autocomplete
+                    id="lane-select"
+                    options={laneOptions}
                     value={lane}
                     onChange={handleChangeLane}
-                    label="Select Lane"
-                >
-                    <MenuItem value={'middle'}>Middle</MenuItem>
-                    <MenuItem value={'top'}>Top</MenuItem>
-                    <MenuItem value={'jungle'}>Jungle</MenuItem>
-                    <MenuItem value={'bottom'}>Bottom</MenuItem>
-                    <MenuItem value={'support'}>Support</MenuItem>
-                </Select>
+                    renderInput={(params) => <TextField {...params} label="Select Lane" />}
+                />
             </FormControl>
 
-            <FormControl fullWidth>
+            <FormControl fullWidth style={{ marginBottom: '10px' }}>
                 <Autocomplete
                     multiple
                     id="champion-select"
@@ -107,7 +108,7 @@ const ChampionsPage = () => {
                 />
             </FormControl>
 
-            <FormControl fullWidth>
+            <FormControl fullWidth style={{ marginBottom: '10px' }}>
                 <Autocomplete
                     multiple
                     id="tiers-select"
@@ -125,9 +126,40 @@ const ChampionsPage = () => {
                 label="Win Rate Numeric Threshold"
                 value={winRateNumericThreshold}
                 onChange={handleChangeWinRateNumericThreshold}
+                style={{ marginBottom: '10px' }}
             />
 
-            <Button variant="contained" onClick={handleSaveSelectedChampions}>Save Selected Champions</Button>
+            <Button variant="contained" onClick={handleSaveSelectedChampions} style={{ backgroundColor: '#FFA500', color: '#fff' }}>Save Selected Champions</Button>
+
+            {/* Exibindo a tabela de campeões inseridos */}
+            {insertedChampions.length > 0 && (
+                <TableContainer component={Paper} style={{ marginTop: '20px' }}>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Name</TableCell>
+                                <TableCell>Lane</TableCell>
+                                <TableCell>Win Rate</TableCell>
+                                <TableCell>Tier</TableCell>
+                                <TableCell>Pick Rate</TableCell>
+                                <TableCell>Ban Rate</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {insertedChampions.map((champion, index) => (
+                                <TableRow key={index}>
+                                    <TableCell>{champion.name}</TableCell>
+                                    <TableCell>{champion.lane}</TableCell>
+                                    <TableCell>{champion.winRate}</TableCell>
+                                    <TableCell>{champion.tier}</TableCell>
+                                    <TableCell>{champion.pickRate}</TableCell>
+                                    <TableCell>{champion.banRate}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            )}
         </div>
     );
 };
